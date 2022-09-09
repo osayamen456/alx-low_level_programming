e1b0bb7b7bbbe9da206dda16bb84bd0caa60dea7
osayamen456 <107184414+osayamen456@users.noreply.github.com>
2022-09-09 16:15:55 +0100

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: 1 (success)
+ *
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Kopa, \
+	2015-10-19\n");
+	return (1);
+}
