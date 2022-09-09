cc78d3e29e1fd90078c433d59d3ac6daef4c5738
osayamen456 <107184414+osayamen456@users.noreply.github.com>
2022-09-09 16:14:20 +0100

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the intro function.
+ *
+ *
+ * Return: 0 (sucesss)
+ */
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
