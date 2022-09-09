6129cedad01a197f9214ed3f8ffb19ae7a48eaaa
osayamen456 <107184414+osayamen456@users.noreply.github.com>
2022-09-09 16:14:51 +0100

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - normal main function
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
